e55065b6055d3978e5e8f7ed872dd84afb2f5e11
Konstantin Zhuravlev <113041604+constZhur@users.noreply.github.com>
2025-03-19 16:29:41 +0300

    Иницилизация проекта Fantasy Football

---
+++ src/main/java/ru/mirea/fantasyfootballbackend/FantasyFootballBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.mirea.fantasyfootballbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyFootballBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantasyFootballBackendApplication.class, args);
+    }
+
+}
